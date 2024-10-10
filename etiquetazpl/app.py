d79c0c8331333192fa6518089a8f2536b223c36f
SergioWB <sergio@wonderbrands.co>
2024-10-09 20:27:57 -0600

    [UPD] Logica con campo select_carrier 3

--- etiquetazpl/app.py
+++ etiquetazpl/app.py
@@ -260,7 +260,7 @@ def get_order_id(name):
                                              "args": [db_name, user_id, password, "sale.order", "search_read",
                                                       search_domain,
                                                       ['channel_order_reference', 'name', 'yuju_seller_id',
-                                                       'yuju_carrier_tracking_ref', 'team_id']]}})
+                                                       'yuju_carrier_tracking_ref', 'team_id', 'client_order_ref']]}})
             res = requests.post(json_endpoint, data=payload, headers=headers).json()
             # logging.info(default_code+str(res))
             # print (res)
