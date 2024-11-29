b5b6db85d0f406d25c77fcc205187fd1f9f9900d
SergioWB <sergio@wonderbrands.co>
2024-11-29 12:19:18 -0600

    [UPD] Cambio de local path para pruebas

--- etiquetazpl/app.py
+++ etiquetazpl/app.py
@@ -677,11 +677,11 @@ def recupera_meli_token(user_id):
         token_dir = ''
 
         if user_id == 25523702:  # Usuario de SOMOS REYES VENTAS
-            token_dir = '/home/ubuntu/Documents/server-Tln/Tools/meli/tokens_meli.txt'
+            token_dir = '/home/sergio/servidor-Tlalne/meli/tokens_meli.txt'   # '/home/ubuntu/Documents/server-Tln/Tools/meli/tokens_meli.txt'
         elif user_id == 160190870:  # Usuario de SOMOS REYES OFICIALES
-            token_dir = '/home/ubuntu/Documents/server-Tln/Tools/meli/tokens_meli_oficiales.txt'
+            token_dir = '/home/sergio/servidor-Tlalne/meli/tokens_meli_oficiales.txt'   # '/home/ubuntu/Documents/server-Tln/Tools/meli/tokens_meli_oficiales.txt'
         elif user_id == 1029905409:  # Usuario de SKYBRANDS
-            token_dir = '/home/ubuntu/Documents/server-Tln/Tools/meli/tokens_meli_skyBrands.txt'
+            token_dir = '/home/sergio/servidor-Tlalne/meli/tokens_meli_skyBrands.txt'  # '/home/ubuntu/Documents/server-Tln/Tools/meli/tokens_meli_skyBrands.txt'
 
         # print token_dir
 
