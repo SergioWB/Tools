93faf7d21b63611851d611ef37b609b46fafe214
SergioWB <sergio@wonderbrands.co>
2024-12-05 20:02:30 -0600

    [UPD] Aniade casos ML con guias en VALPICK

--- etiquetazpl/app.py
+++ etiquetazpl/app.py
@@ -936,7 +936,7 @@ def procesar():
                     print_label_case = 'NO ENCONTRADO'
 
                 # SE INCLUYEN LOS CASOS DE MARKETPLACES CON ETIQUETAS VALIDAS (a parte de Fedex)
-                if (label_type_carrier_logic != False and team_id.lower() != "team_mercadolibre") or (
+                if (label_type_carrier_logic != False and team_id.lower() != "team_mercadolibre") or (team_id.lower() == 'team_mercadolibre' and guide_number.lower() not in ['colecta','flex','drop off'] and label_type_carrier_logic != 'PaquetExpress') or (
                             'fedex' in guide_number.lower() or label_case_guide_number_logic in [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]):  # Si el caso está en los carriers existentes en la lista
                     #if team_id.lower() == 'team_elektra' or team_id.lower() == 'team_mercadolibre':  # team_id.lower() == 'team_liverpool' or
                         #respuesta = f'¡ESTA  ORDEN  ES  DE  "{team_id.upper()}"  CON  GUIA  DE  FeDex,  FAVOR  DE  IMPRIMIR  EN  ODOO!'
