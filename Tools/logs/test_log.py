1eaa0565f33391e74de78c96768d59fc05e45b9e
SergioWB <sergio@wonderbrands.co>
2025-06-10 12:22:10 -0600

    Modifica test_log.py

--- Tools/logs/test_log.py
+++ Tools/logs/test_log.py
@@ -1,13 +1,16 @@
 from log_config import logger_config
 import time
 
-logger = logger_config("meli_insercion", r"/home/ubuntu/Documents/server-Tln/Tools/Tools/logs")
+logger = logger_config("meli_insercion", r"/home/ubuntu/Documents/server-Tln/Tools/Tools/logs/meli_insercion.log")
+#logger = logger_config("meli_insercion", r"C:\Users\Sergio Gil Guerrero\Documents\WonderBrands\Repos\Tools\Tools\logs\meli_insercion.log")
 
 try:
     for i in range(0,9):
         print(f"{i+1}")
         time.sleep(1)
 
-    raise ('Exception')
+        if i == 7:
+            raise ('Exception')
+
 except Exception as e:
     logger.error(f'Error recupera_meli_token(): {str(e)}')
